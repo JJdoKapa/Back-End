66babe08818918d93a494f6012e6f9b3ae1271c9
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-03-19 08:26:26 -0300

    Create exe09.py

---
+++ Exercicíos/exe09.py
@@ -0,0 +1,4 @@
+metros = float(input("quantos metros: "))
+centimetros = metros*100
+melimetros = metros*1000
+print(f"{metros} é equivalente a {centimetros} centimetros e melimetros é equivalente a {melimetros} melimetros!")
