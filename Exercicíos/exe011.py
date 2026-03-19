a1fda931182cfe8dd0b82e9112c33f385e8208fa
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-03-19 08:58:07 -0300

    Create exe011.py

---
+++ Exercicíos/exe011.py
@@ -0,0 +1,3 @@
+carteira = float(input("digite a quantidade de dinheiro desejada: "))
+dolar = carteira/5.24
+print("está quantia de dinheiro em reais: {:.0f}. É equivalente a: {:.0f} dólares".format(carteira,dolar))
