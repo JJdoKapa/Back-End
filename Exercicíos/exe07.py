dfae6c172b1d76b2e5910d02d3d0462cd3276000
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-03-19 08:05:27 -0300

    Create exe07.py

---
+++ Exercicíos/exe07.py
@@ -0,0 +1,5 @@
+numero = int(input("digite um número:"))
+dobro = numero*2
+triplo = numero*3
+raiz = numero**0.5
+print(f"o número digitado foi:{numero}, o dobro é:{dobro}, o triplo é:{triplo} e a raiz é:{raiz}")
