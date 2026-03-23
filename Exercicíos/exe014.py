e8a4efeb7dc9aab6b8575f58ec7847535c6ea842
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-03-23 10:56:20 -0300

    Create exe014.py

---
+++ Exercicíos/exe014.py
@@ -0,0 +1,3 @@
+salario = float(input("digite o seu salário: "))
+aumento = salario*1.15
+print("o e salário é: {:.0f}$. Mas com o aumento de 15%, seu salário agora é: {:.0f}$". format(salario, aumento))
