06e88235cecaaa67cce82b95eb4743ac724507be
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-03-26 08:26:46 -0300

    Create Cont007.py

---
+++ Conteudo/Cont007.py
@@ -0,0 +1,4 @@
+BIBLIOTECA MATH
+
+#Essa biblioteca é bem usada em python e seu nome significa matemática.
+#Quando importada ela traz diversos recursos matemáticos como exemplo a funcionalidade ceil que arredonda números para cima ou a funcionalidade floor que realiza o arredondamento pra baixo.
