fb17d9e1b0f049de9fa9464e2f8e909392706456
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-04-28 12:23:01 -0300

    Create exe020.py

---
+++ Exercicíos/exe020.py
@@ -0,0 +1,8 @@
+from random import choice
+aluno1=str(input("digite o nome do 1º aluno: "))
+aluno2=str(input("digite o nome do 2º aluno: "))
+aluno3=str(input("digite o nome do 3º aluno: "))
+aluno4=str(input("digite o nome do 4º aluno: "))
+lista = [aluno1, aluno2, aluno3, aluno4]
+choice(lista)
+print("a ordem do sorteio é: {}".format(lista))
