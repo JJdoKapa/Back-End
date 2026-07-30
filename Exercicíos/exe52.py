b6c392f6a14277c362ebeb854bcee2de352bdbea
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-07-30 08:50:48 -0300

    Create exe52.py

---
+++ Exercicíos/exe52.py
@@ -0,0 +1,14 @@
+maior = 0
+menor = 0
+for c in range(1,6):
+    p = float(input(f'pesoda {c} pessoa em kg: '))
+    if p == 1:
+        maior = p
+        menor = p
+    else:
+        if p > maior:
+            maior = p
+        elif p < menor:
+            menor = p
+print(f'o maior peso lido foi {maior}kg')
+print(f'o menor peso lido foi {menor}kg')
