a4630d4e6c8da80e8eae93ec60d5dcc1fe4fa13e
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-08-26 10:55:02 -0300

    Delete Exercicíos/ex013.py

--- Exercicíos/ex013.py
+++
@@ -1,3 +0,0 @@
-preço = float(input("Digite o preço do produto:"))
-preçodescontado = preço - (preço*5/100)
-print("O produto custava:{:.0f}, mas com o desconto de 5% ficará: {:.0f}".format(preço, preçodescontado))
