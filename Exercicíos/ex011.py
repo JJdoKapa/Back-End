b29f31332a84981263de2cd51f68525071505422
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-08-26 10:54:44 -0300

    Delete Exercicíos/ex011.py

--- Exercicíos/ex011.py
+++
@@ -1,3 +0,0 @@
-dindin = float(input("Digite a quantia de dinheiro desejada:"))
-dolar = dindin/5.24
-print("Esta quantia de dinheiro em reais: {:.0f} é equivalente a {:.0f} em dólares.".format(dindin, dolar))
