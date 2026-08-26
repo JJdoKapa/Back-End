6a02715dbe3c4dc0774e0b343dcb9d7daa53402b
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-08-26 10:55:12 -0300

    Delete Exercicíos/ex014.py

--- Exercicíos/ex014.py
+++
@@ -1,3 +0,0 @@
-SalarioAtual = float (input("Digite seu salário:"))
-SalarioNovo = (SalarioAtual*15/100)
-print("Seu salário era: {:.2f}, o novo salário será: {:.2f}".format(SalarioAtual, SalarioNovo))
