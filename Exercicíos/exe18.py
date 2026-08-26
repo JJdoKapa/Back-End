57b7bae64ff464c1152a4cf6e4164aadfe2ee801
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-08-26 10:45:15 -0300

    Delete Exercicíos/exe18.py

--- Exercicíos/exe18.py
+++
@@ -1,4 +0,0 @@
-catetoop = float(input('Comprimento do cateto oposto: '))
-catetoad = float(input('Comprimento do cateto adjacente: '))
-hipotenusa = (catetoop ** 2 + catetoad ** 2) ** (1/2)
-print(f'A hipotenusa vai medir {hipotenusa:.2f}')
