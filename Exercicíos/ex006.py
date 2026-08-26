f2ddd9d22f7f70204146c7d03aa971d371e5eea0
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-08-26 10:53:54 -0300

    Delete Exercicíos/ex006.py

--- Exercicíos/ex006.py
+++
@@ -1,5 +0,0 @@
-numero = int(input("Digite um número inteiro: "))
-antecessor = numero - 1
-sucessor = numero + 1
-print(f"Analisando o valor {numero}...")
-print(f"O seu antecessor é {antecessor} e o seu sucessor é {sucessor}.")
