6fe987769d7a00cace4bd457d5c48a0e73ad72f8
Jonatas Roberto Rosa Junior <junior.jonatas@escola.pr.gov.br>
2026-08-31 10:34:39 -0300

    Create exe049.py

---
+++ Exercicíos/exe049.py
@@ -0,0 +1,9 @@
+soma = 0
+= 0 cont
+for c in range (1,7):
+num = int(input(f"digite o {c} valor "))
+if num%2 ==0:
+soma += num
+cont = cont +1
+print(f"Você informou {cont} numeros pares e a
+soma dos numeros pares é {soma}")
